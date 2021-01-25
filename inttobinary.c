10ccad91f5bef175eb9e7200c7fa827c63112e1c
jmaus06 <55416199+jmaus06@users.noreply.github.com>
2021-01-24 23:53:12 -0500

    working binary

--- inttobinary.c
+++ inttobinary.c
@@ -4,7 +4,7 @@
 
 int main(){
 
-int i;
+int i,current;
 int bit,bits,bytevalue;
 
 printf("Enter integer: ");
@@ -20,7 +20,21 @@ bytevalue =(pow(2,bits)-1);
 
 } while (i > bytevalue);
 
-printf("\nValue has been determined.\n");
+bits=bits-1;
+while (bits>=0){
+
+	current = pow(2,bits);
+	/*printf("%d ",current);*/
+	if (current<=i){
+		i=i-current;
+		printf("1 ");
+	}
+	else{
+		printf("0 ");
+	}
+	bits = bits -1; /*go to next smallest bit*/
+}
+
 
 return 0;
 }
